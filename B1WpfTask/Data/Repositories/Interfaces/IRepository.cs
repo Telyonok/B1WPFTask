9c620565da7de64211914c03369b1d29921cd8b5
Ilya_Chvilyov <life270317@gmail.com>
2024-08-21 15:17:26 +0300

    Defined repositories for tasks.

---
+++ B1WpfTask/Data/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace B1WPFTask.Data.Repositories.Interfaces
+{
+    internal interface IRepository<T> where T : class
+    {
+        IQueryable<T> Get(Expression<Func<T, bool>> expression);
+        Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        IQueryable<T> GetAll();
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default);
+        Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+        Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
+    }
+}
---
+++ B1WpfTask/Data/Repositories/RandomRowRepository.cs
@@ -0,0 +1,45 @@
+﻿using B1WPFTask.Data.Repositories.Interfaces;
+using B1WPFTask.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace B1WPFTask.Data.Repositories
+{
+    internal class RandomRowRepository<T> : IRepository<T> where T : class, IEntity
+    {
+        private readonly RandomRowDBContext _context;
+
+        public RandomRowRepository(RandomRowDBContext context) => _context = context;
+
+        public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default)
+        {
+            await _context.Set<T>().AddRangeAsync(entities, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public IQueryable<T> Get(Expression<Func<T, bool>> expression) => _context.Set<T>().Where(expression);
+
+        public IQueryable<T> GetAll() => _context.Set<T>();
+
+        public async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default) =>
+            await _context.Set<T>().Where(entity => entity.Id == id).FirstOrDefaultAsync(cancellationToken);
+
+        public async Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            _context.Set<T>().Update(entity);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
